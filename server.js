08302c0285d0359132d4f9d7c6d1543ece2cab1f
SaddamTechie <saddamtechie@gmail.com>
2025-01-21 14:35:39 +0300

    database setup

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Database connection
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
